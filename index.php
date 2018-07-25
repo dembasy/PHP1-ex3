5cd3df3c13710fefdafac92df8bd371b733a7bcb
dembasy <dembasy1227@gmail.com>
2018-07-25 15:38:31 +0200

    done

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex3</title>
+  </head>
+  <body><p><?php $km = 1; $km = 3; $km = 125; echo $km; ?></p>
+
+  </body>
+</html>
